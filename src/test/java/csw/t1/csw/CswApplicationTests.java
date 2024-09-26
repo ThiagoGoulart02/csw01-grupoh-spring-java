2783eb381ebaca2781aae81c1fd4fe47799e70e9
Thiago Goulart <111789880+ThiagoGoulart02@users.noreply.github.com>
2024-09-26 18:18:10 -0300

    Add files via upload

---
+++ src/test/java/csw/t1/csw/CswApplicationTests.java
@@ -0,0 +1,13 @@
+package csw.t1.csw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
