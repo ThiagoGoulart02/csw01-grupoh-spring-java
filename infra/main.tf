73a7544094a35701cba2450be02f1a7968cfff9c
rodrigolopesm <rodrigo.michalski@sap.com>
2024-10-22 10:50:12 -0300

    update infra

---
+++ infra/main.tf
@@ -0,0 +1,103 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "ec2" {
+  ami = "ami-0866a3c8686eaeeba"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.api_access.id]
+  key_name = aws_key_pair.key_pair.key_name
+  tags = {
+    Name = "ec2"
+  }
+}
+resource "aws_security_group" "api_access" {
+  name        = "API-security-group-T1"
+  description = "Security group para permitir SSH, HTTP e HTTPS"
+
+  # Regra de entrada para SSH (porta 22)
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Regra de entrada para HTTP (porta 80)
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Regra de entrada para HTTPS (porta 443)
+  ingress {
+    description = "HTTPS"
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "api port"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "pg port"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "pgAdmin port"
+    from_port   = 5050
+    to_port     = 5050
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Regra de saída que permite todo o tráfego
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Criação da ssh keypair diretamente no terraform
+resource "tls_private_key" "my_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = "ec2-key"
+  public_key = tls_private_key.my_key.public_key_openssh
+}
+
+resource "local_file" "my_key_pem" {
+  filename = "${path.module}/ec2-key.pem"
+  content  = tls_private_key.my_key.private_key_pem
+  file_permission = "0400"
+}
+
+output "private_key_pem" {
+  value     = tls_private_key.my_key.private_key_pem
+  sensitive = true
+}
+
+output "aws_instance_public_ip" {
+  value = aws_instance.ec2.public_ip
+}
