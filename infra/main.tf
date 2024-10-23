597bea7e54bd1bc3435d28a4069e31e6a4704dd5
rodrigolopesm <rodrigolopesmichalski@gmail.com>
2024-10-22 22:51:07 -0300

    fix infra

--- infra/main.tf
+++ infra/main.tf
@@ -11,22 +11,46 @@ resource "aws_instance" "ec2" {
     Name = "ec2"
   }
   user_data = <<-EOF
-        #!/bin/bash
-        sudo apt-get update -y
-        sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-        sudo apt-get update -y
-        sudo apt-get install -y docker-ce
-        sudo usermod -aG docker ubuntu
-        sudo systemctl enable docker
-        sudo systemctl start docker
+              #!/bin/bash
+              # Update package index
+              sudo apt-get update -y
+              sudo ufw allow 8080
 
-        # git clone https://github.com/ThiagoGoulart02/csw01-grupoh-spring-java.git
-        # cd csw01-grupoh-spring-java/docker/
-        # docker compose up -d
+              # Install Docker
 
-    EOF
+              sudo apt-get update
+              sudo apt-get install ca-certificates curl
+              sudo install -m 0755 -d /etc/apt/keyrings
+              sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+              sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+              # Add the repository to Apt sources:
+              echo \
+                "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+                $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+                sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+              sudo apt-get update
+
+              # Install Docker Engine
+              sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+              # Start Docker service
+              sudo systemctl start docker
+              sudo systemctl enable docker
+
+              # Clone github
+              mkdir repo
+              cd repo
+              git clone https://github.com/ThiagoGoulart02/csw01-grupoh-spring-java.git
+
+              # Enter repo
+              cd csw01-grupoh-spring-java
+              cd docker
+
+              # Build Docker image
+              sudo docker compose up -d
+              EOF
+
 }
 resource "aws_security_group" "api_access" {
   name        = "API-security-group-T1"
